3574e6e1e616cf2b457e7a5f15dda7ecd078414c
Leon Qin <qinling.net@gmail.com>
2017-05-28 23:21:05 +0800

    print out event type.

--- main.ts
+++ main.ts
@@ -4,12 +4,14 @@ class EventBroker {
     public static main(): number {
 
         let app = express();
-        app.get('/', function(req, res){
+        app.get('/', function (req, res) {
             res.send('hello world');
             console.log('hello world');
         });
-	
-	app.post('/events', function(req, res) {
+
+        app.post('/events', function (req, res) {
+            let evtType = req.get('X-GitHub-Event');
+            console.log(evtType);
             res.sendStatus(201);
         });
         app.listen('8081');
